20faacbd64b705e65166b28fa805dba68f104653
Xahyx7 <mohdzahikzafar@gmail.com>
2026-03-22 12:41:21 +0530

    Update history.js

--- api/history.js
+++ api/history.js
@@ -1,24 +1,29 @@
 import clientPromise from "./db";
 
 export default async function handler(req, res) {
-  const { username, action, data } = req.body;
+  try {
+    const { username, action, data } = req.body;
 
-  const client = await clientPromise;
-  const db = client.db("simplifyai");
-  const users = db.collection("users");
+    const client = await clientPromise;
+    const db = client.db("simplifyai");
+    const users = db.collection("users");
 
-  if (action === "save") {
-    await users.updateOne(
-      { username },
-      { $push: { history: data } }
-    );
+    if (action === "save") {
+      await users.updateOne(
+        { username },
+        { $push: { history: data } }
+      );
 
-    return res.json({ success: true });
-  }
+      return res.json({ success: true });
+    }
 
-  if (action === "get") {
-    const user = await users.findOne({ username });
+    if (action === "get") {
+      const user = await users.findOne({ username });
+      return res.json({ history: user?.history || [] });
+    }
 
-    return res.json({ history: user?.history || [] });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Server error" });
   }
 }
